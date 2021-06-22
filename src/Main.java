0d91f5e37ccac1d4ffc0e6ea400e43b3ad857b8b
hung060798 <hungpppp122gmail.com>
2021-06-22 11:20:29 +0700

    up

---
+++ src/Main.java
@@ -0,0 +1,24 @@
+public class Main {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        Queue q = new Queue();
+        q.setFront(null);
+        q.setRear(null);
+
+        solution.enQueue(q, 14);
+        solution.enQueue(q, 22);
+        solution.enQueue(q, 6);
+
+        solution.displayQueue(q);
+
+        System.out.println("Deleted value = " + solution.deQueue(q));
+        System.out.println("Deleted value = " + solution.deQueue(q));
+
+        solution.displayQueue(q);
+
+        solution.enQueue(q, 9);
+        solution.enQueue(q, 20);
+        solution.displayQueue(q);
+
+    }
+}
